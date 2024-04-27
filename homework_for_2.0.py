974a9fbac4b62c29a2ad70f02d1ce17ceda20510
kefirr85 <166715341+kefirr85@users.noreply.github.com>
2024-04-27 19:07:06 +0300

    Add files via upload

---
+++ homework_for_2.0.py
@@ -0,0 +1,8 @@
+list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+#str = 'Я езжу на автомабиле марки '
+cars_count = 0
+for i in range(len(list)):
+    cars_count += 10
+    print(f'Я езжу на автомабиле марки: {list[i]} {cars_count}')
+#    print(cars_count)
+#    print(str, i)
